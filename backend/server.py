3634622536e8c785f933480e38c169efd87e0241
Weichen Liu <weicliucs@gmail.com>
2016-12-12 12:00:23 -0800

    mongodb client

--- backend/server.py
+++ backend/server.py
@@ -1,7 +1,17 @@
+import os, sys
 import pyjsonrpc
+import json
+from bson.json_util import dumps
+
+# import common package in parent dir
+sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'common'))
+
+import mongodb_client
 
 SERVER_HOST = 'localhost'
 SERVER_PORT = 4040
+
+PROPERTY_TABLE_NAME = 'property'
 
 class RequestHandler(pyjsonrpc.HttpRequestHandler):
     """Test method"""
@@ -12,15 +22,17 @@ class RequestHandler(pyjsonrpc.HttpRequestHandler):
 
     @pyjsonrpc.rpcmethod
     def searchArea(self, query):
+        res = []
         if query.isdigit():
-            # TODO: search in db for zipcode
-            pass
+            db = mongodb_client.getDB()
+            res = db[PROPERTY_TABLE_NAME].find({'zipcode': query})
+            res = json.loads(dumps(res)) # BSON -> string -> JSON
         else:
             query_split = query.split(',')
             city = query_split[0].strip()
             state = query_split[1].strip()
             # TODO: search in db
-        return ["House_1", "Condo_2"]
+        return res
 
 
 http_server = pyjsonrpc.ThreadingHttpServer(
